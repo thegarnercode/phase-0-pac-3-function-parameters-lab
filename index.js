d864ff6fcec98d35b3ce2e08b221f596b4d76560
Denisha Garner <thegarnercode@gmail.com>
2022-03-28 00:09:28 -0400

    commit tree 7c9bec8c69d268bd49455d84091d51f60a8295e2

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
+}
+introduction("aki");
+
+function introductionWithLanguage(name, language) {
+return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+introductionWithLanguage("Aki", "Ember.js")
+
+
+function introductionWithLanguageOptional(name, language = "Javascript") {
+  return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+  }
+  introductionWithLanguage("Gracie")
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+    }
+    introductionWithLanguage("Gracie", "JavaScript")
